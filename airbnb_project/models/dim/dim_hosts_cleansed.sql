53e963160235f0c71a4d323525cccf4df5b5ecdb
sheikhDataScientist <sheikh.4189@gmail.com>
2026-05-16 08:51:10 +0000

    dim first commit

---
+++ airbnb_project/models/dim/dim_hosts_cleansed.sql
@@ -0,0 +1,17 @@
+WITH result_set AS (
+    SELECT
+        *
+    FROM
+        {{ ref('src_hosts') }}
+)
+SELECT
+    host_id,
+    NVL(
+        host_name,
+        'Anonymous'
+    ) AS host_name,
+    is_superhost,
+    created_at,
+    updated_at
+FROM
+    result_set
